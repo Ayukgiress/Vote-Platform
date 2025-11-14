3352884a315b0006ef132d4de6d9215a77c98a50
Ayuk <giress865@gmail.com>
2025-11-14 13:43:50 +0100

    updated the contest cards designs

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -9,9 +9,7 @@ const Dashboard = () => {
 
       <main className="flex-1 flex flex-col overflow-hidden">
         <div className="flex-1 overflow-auto p-6 lg:p-8">
-          <div className="max-w-7xl mx-auto">
-            <Outlet />
-          </div>
+          <Outlet />
         </div>
       </main>
     </div>
